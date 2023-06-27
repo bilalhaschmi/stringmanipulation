b5b14cbd356f5b53078489f2ede03872fe2a608b
bhaschmi <bilal.haschmi@googlemail.com>
2023-06-27 12:12:42 +0200

    added upAndDown func and test "hello" -> "hElLo"

--- stringmanipulation.go
+++ stringmanipulation.go
@@ -5,3 +5,19 @@ import "strings"
 func ToUppercase(input string) string {
 	return strings.ToUpper(input)
 }
+
+func TrimIt(input string) string {
+	return strings.TrimSpace(input)
+}
+
+func UpAndDown(input string) string {
+	result := ""
+	for i, char := range input {
+		if i%2 == 0 {
+			result += strings.ToUpper(string(char))
+		} else {
+			result += strings.ToLower(string(char))
+		}
+	}
+	return result
+}
--- stringmanipulation_test.go
+++ stringmanipulation_test.go
@@ -21,3 +21,25 @@ func TestToUppercase(t *testing.T) {
 		t.Errorf("ToUppercase(%s) = %s, expected %s", emptyString, resultEmpty, expectedEmpty)
 	}
 }
+
+func TestTrim(t *testing.T) {
+	inputString := "   hello   "
+	expectedString := "hello"
+
+	result := TrimIt(inputString)
+
+	if result != expectedString {
+		t.Errorf("Trim(%s) = %s, expectedString %s", inputString, result, expectedString)
+	}
+}
+
+func TestUpAndDown(t *testing.T) {
+	inputString := "hello world"
+	expectedString := "HeLlO wOrLd"
+
+	result := UpAndDown(inputString)
+
+	if result != expectedString {
+		t.Errorf("UpAndDown(%s) = %s, expectedString %s", inputString, result, expectedString)
+	}
+}
